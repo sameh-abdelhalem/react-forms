f868c48c78948a021ae28f6504cb6bf50a1e736e
Sameh <sameh.abdelhaleem95@gmail.com>
2022-09-19 20:27:45 +0200

    read the changed input via state

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,28 @@
+import { computeHeadingLevel } from "@testing-library/react";
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    setEnteredName("");
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          value={enteredName}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
